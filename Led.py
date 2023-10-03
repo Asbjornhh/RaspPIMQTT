c3f4ea015879b02c6896aa96f52a445d30e07f83
Asbjornhh <89381324+Asbjornhh@users.noreply.github.com>
2023-10-03 21:05:00 +0200

    time-sleep3

--- Led.py
+++ Led.py
@@ -1,26 +1,10 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import LED
+from time import sleep
 
-# Set the GPIO numbering mode to BCM
-GPIO.setmode(GPIO.BCM)
+led = LED(17)  # Change the number to the GPIO pin you're using
 
-# Define the GPIO pin where the LED is connected
-LED_PIN = 17
-
-# Set the LED_PIN as an output pin
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-try:
-    while True:
-        # Turn the LED on
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(1)  # Keep the LED on for 1 second
-
-        # Turn the LED off
-        GPIO.output(LED_PIN, GPIO.LOW)
-        time.sleep(1)  # Keep the LED off for 1 second
-
-except KeyboardInterrupt:
-    # If the script is interrupted (e.g., pressing Ctrl+C), turn off the LED and cleanup the GPIO
-    GPIO.output(LED_PIN, GPIO.LOW)
-    GPIO.cleanup()
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
