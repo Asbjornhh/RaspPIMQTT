41eb7355a76044646c7481bc064a050e2fb0f63d
Asbjornhh <89381324+Asbjornhh@users.noreply.github.com>
2023-10-05 22:57:59 +0200

    humidity.py

--- humidity.py
+++ humidity.py
@@ -1,13 +1,17 @@
 import Adafruit_DHT
+import time  # <-- import the time module
 
 # Sensor type
 DHT_SENSOR = Adafruit_DHT.DHT11
 # GPIO pin number (not the physical pin number)
 DHT_PIN = 23
 
-humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+while True:  # <-- start an infinite loop
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
-if humidity is not None and temperature is not None:
-    print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
-else:
-    print("Failed to retrieve data from the sensor")
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Failed to retrieve data from the sensor")
+
+    time.sleep(2)  # <-- delay for 2 seconds before the next read
